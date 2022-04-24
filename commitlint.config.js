32d37714b20beba4c45cafa863ca1667cbbf4f45
sufan <1301756936@qq.com>
2022-04-24 14:51:17 +0800

    tool(其它变更): feat(其它变更): 测试coomit规范
    这是一个详细说明

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+odule.exports = {
+  extends: ['cz'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(.*?)((.*?)):\s?(.*)$/,
+      headerCorrespondence: ['type', 'scope', 'subject'],
+    },
+  },
+  rules: {
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+  },
+};
